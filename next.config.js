290b62d7b8a5aabc457213b950d99ebdcf1ad4cb
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2019-05-11 20:51:46 +0530

    setting production config and deploy using now

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? {}
     : !process.env.NOW_REGION
-      ? require('next/constants')
-      : require('next-server/constants');
+    ? require("next/constants")
+    : require("next-server/constants");
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
-    return {};
+    return {
+      target: "serverless"
+    };
   }
 
   // ✅ Put the require call here.
-  const withCSS = require('@zeit/next-css');
+  const withCSS = require("@zeit/next-css");
 
   return withCSS();
 };
