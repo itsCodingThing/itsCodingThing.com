9e32d0b9a7d30e57159ca7e72f90cb887e9e8915
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2023-11-24 19:20:05 +0530

    update assets url

--- src/app/@about/page.tsx
+++ src/app/@about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
             <div className="flex justify-center items-center">
                 <Image
                     className="border-8 border-yellow-500 rounded"
-                    src="/myPic2-fixed.jpg"
+                    src="/assets/myPic2.jpg"
                     alt="my-pic"
                     width={300}
                     height={500}
@@ -17,7 +17,7 @@ export default function AboutPage() {
             <div className={`text-[#f5deb3] flex flex-col justify-center ${robotoCondensed.className}`}>
                 <div className="mb-4 flex items-center">
                     <h1 className="text-8xl font-semibold">Hi,</h1>
-                    <Image src="/hi2.png" alt="sayHi" width={100} height={100} />
+                    <Image src="/assets/hi2.png" alt="sayHi" width={100} height={100} />
                 </div>
                 <h1 className="text-3xl font-bold mb-2">&lt;FullStackDeveloper&#47;&gt;</h1>
                 <div className="text-lg font-medium">
