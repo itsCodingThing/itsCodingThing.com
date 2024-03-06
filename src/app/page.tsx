258afdbad0360160fb0f575a4ca7259b73fc49ae
Bhanu Pratap Singh <30897910+itsCodingThing@users.noreply.github.com>
2024-03-06 23:05:27 +0530

    Update page.tsx
    remove commented MagicTyped component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// import MagicTyped from "@/components/MagicTyped";
 import ScollArrow from "@/components/DownArrow";
 import MagicTyping from "@/components/MagicTyping";
 
@@ -6,7 +5,6 @@ export default function HomePage() {
     return (
         <section id="intro" className="min-h-screen relative">
             <div className="h-screen grid place-items-center">
-                {/* <MagicTyped /> */}
                 <MagicTyping lines={["itsCodingThing", "I am developer"]}/>
             </div>
             <ScollArrow to="#about" />
