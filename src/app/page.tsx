f45328c431dc7416eda339d543b6d68237bf83ff
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-03-07 18:44:36 +0530

    update packages and resolving eslint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     return (
         <section id="intro" className="min-h-screen relative">
             <div className="h-screen grid place-items-center">
-                <MagicTyping lines={["itsCodingThing", "I am developer", "I am foodie"]}/>
+                <MagicTyping lines={["itsCodingThing", "I am developer", "I am foodie"]} />
             </div>
             <ScollArrow to="#about" />
         </section>
