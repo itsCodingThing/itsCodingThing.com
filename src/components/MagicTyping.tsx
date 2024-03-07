8215abd17f066e083a2be3b14fcd37374170c2c4
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-03-07 18:17:27 +0530

    1. update global.d.ts file 2. remove the typed.js package 3 added own typed.js implementation(not completed yet)

--- src/components/MagicTyping.tsx
+++ src/components/MagicTyping.tsx
@@ -4,60 +4,64 @@ import { useEffect, useState } from "react";
 
 interface IMagicTypingProps {
     lines: string[];
+    smartBackspace?: boolean;
 }
 
 interface IMagicTypingState {
-    lines: string;
+    text: string;
     index: number;
     reverse: boolean;
 }
 
-export default function MagicTyping({ lines }: IMagicTypingProps) {
-    const [state, setState] = useState<IMagicTypingState>({ lines: "", index: 0, reverse: false });
-
+export default function MagicTyping(props: IMagicTypingProps) {
+    const { lines } = props;
+    const [state, setState] = useState<IMagicTypingState>({ text: "", index: 0, reverse: false });
+    
     useEffect(() => {
         const interval = setInterval(() => {
             if (state.reverse) {
+                // going backwards
                 setState((prev) => {
-                    const newText = state.lines.slice(0, state.lines.length - 1);
-             
-                    if (state.lines.length === 0) {
-                        let index = state.index;
+                    const newText = state.text.slice(0, state.text.length - 1);
 
+                    if (state.text.length === 0) {
+                        let index = state.index;
+                        
                         if (index === lines.length - 1) {
                             index = 0;
                         } else {
                             index = index + 1;
                         }
-
-                        return { ...prev, lines: newText, reverse: !prev.reverse, index };
+                        
+                        return { ...prev, text: newText, reverse: !prev.reverse, index };
                     }
-
-                    return { ...prev, lines: newText };
+                    
+                    return { ...prev, text: newText };
                 });
             } else {
+                // going forwards
                 setState((prev) => {
-                    const newText = lines[state.index].slice(0, state.lines.length + 1);
-             
-                    if (state.lines.length === lines[state.index].length) {
-                        return { ...prev, lines: newText, reverse: !prev.reverse };
+                    const newText = lines[state.index].slice(0, state.text.length + 1);
+                    
+                    if (state.text.length === lines[state.index].length) {
+                        return { ...prev, text: newText, reverse: !prev.reverse };
                     }
-
-                    return { ...prev, lines: newText };
+                    
+                    return { ...prev, text: newText };
                 });
             }
         }, 100);
-
+        
         return () => {
             clearInterval(interval);
         };
-    }, [lines, state.index, state.lines, state.reverse]);
-
+    }, [lines, state.index, state.text, state.reverse]);
+    
     return (
         <h1 className="text-4xl md:text-6xl text-secondary">
             <span>
-                {state.lines}
-                <span>|</span>
+                {state.text}
+                <span className="cursor-blink">|</span>
             </span>
         </h1>
     );
